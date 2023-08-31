53048657c8ec1be4f7f0e05ac35e57665fbd1bd3
mateuse <emateus71@gmail.com>
2023-08-31 10:10:44 -0700

    start of carousel functionality

---
+++ wheel-snipe-celly/themes/blog-theme/static/js/carousel.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const popularCarousel = document.querySelectorAll(".app__popular-carousel");
+    
+    popularCarousel.forEach(function(carousel) {
+        console.log("HERO")
+    })
+});
